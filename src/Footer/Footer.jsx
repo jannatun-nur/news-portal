fe54dbd0fbf34fe11de697da72a43a582d3b6643
jannatun-nur <jannatunur03@gmail.com>
2024-06-19 10:36:49 +0600

    footer 100%

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,19 +2,21 @@
 
 const Footer = () => {
     return (
-        <div>
-          <div>
+        <div className="bg-gradient-to-r from-indigo-800 via-blue-600 to-blue-400 text-white
+        p-10">
+          <div className="lg:flex lg:justify-between md:flex md:justify-between columns-1">
             {/* sec for left */}
             <section>
-             <p>Visit Our More Services</p>   
-             <p>www.dailynews.org.com</p>   
-             <p>www.healthcare.org.com</p>   
-             <p>www.firstaid.org.com</p>   
+             <p className="text-2xl font-semibold">Visit Our More Services</p>   
+             <p className="py-2 text-xl">www.dailynews.org.com</p>   
+             <p className="text-xl">www.healthcare.org.com</p>   
+             <p className="py-2 text-xl">www.firstaid.org.com</p>   
             </section>
 
             {/* sec for right */}
-            <section>
-            <p>Subscribe Us</p>
+            <section className="mt-10 md:mt-0 lg:mt-0">
+            <p className="text-2xl font-semibold">Subscribe Us</p>
+            <input className="bg-white rounded-lg text-blue-500" placeholder="@gmail.com"/>
             </section>
           </div>  
         </div>
